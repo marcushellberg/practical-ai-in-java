97948164aa59e6308e8bb9f2cdd222a826edb29a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-19 22:23:41 +0200

    Preparation for live demos
    - Separate example for Guardrail
    - Separate example for Simple RAG
    - Added additional document for RAG example

--- src/main/java/com/example/practicalai/view/SimpleRag.java
+++ src/main/java/com/example/practicalai/view/SimpleRag.java
@@ -15,13 +15,9 @@ import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.Route;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
-import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
-import org.springframework.ai.rag.preretrieval.query.transformation.CompressionQueryTransformer;
-import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -30,63 +26,23 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 
 import java.time.Instant;
-import java.util.UUID;
 
-@Menu(title = "RAG Chat", order = 7)
-@Route("rag-chat")
-public class RagChat extends VerticalLayout {
+@Menu(title = "Simple RAG", order = 6)
+@Route("simple-rag")
+public class SimpleRag extends VerticalLayout {
 
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
-    private final String chatId = UUID.randomUUID().toString();
 
-    public RagChat(ChatClient.Builder builder, VectorStore vectorStore, ChatMemory chatMemory) {
+    public SimpleRag(ChatClient.Builder builder, VectorStore vectorStore) {
         this.vectorStore = vectorStore;
         chatClient = builder
-            .defaultAdvisors(
-                MessageChatMemoryAdvisor.builder(chatMemory).build(),
-                RetrievalAugmentationAdvisor.builder()
-
-                    ////// 1. Pre-Retrieval
-
-                    .queryTransformers(
-                        // Rewrite question into a standalone question based on chat history. Example:
-                        // Original: What are the benefits?
-                        // Rewritten: What are the benefits of exercising?
-                        CompressionQueryTransformer.builder()
-                            .chatClientBuilder(builder.clone())
-                            .build(),
-
-                        // Rewrite the query to optimize for vector search. Example:
-                        // Original: I've considered starting to run or maybe lift weights, are there benefits to that?
-                        // Rewritten: What are the benefits of exercising?
-                        RewriteQueryTransformer.builder()
-                            .chatClientBuilder(builder.clone())
-                            .build()
-                    )
-
-                    // Create additional queries based on the original query to gather more relevant information. Example:
-                    // Original: What are the benefits of exercising?
-                    // New queries:
-                    // - What are the benefits of exercising?
-                    // - What are the benefits of cardio?
-                    // - What are the benefits of strength training?
-                    .queryExpander(MultiQueryExpander.builder()
-                        .chatClientBuilder(builder.clone())
+                .defaultAdvisors(RetrievalAugmentationAdvisor.builder()
+                        .documentRetriever(VectorStoreDocumentRetriever.builder()
+                                .vectorStore(vectorStore)
+                                .build())
                         .build())
-
-                    ////// 2. Retrieval
-                    .documentRetriever(VectorStoreDocumentRetriever.builder()
-                        .vectorStore(vectorStore)
-                        .build())
-
-                    ////// 3. Post-retrieval
-                    .queryAugmenter(ContextualQueryAugmenter.builder()
-                        .allowEmptyContext(true)
-                        .build())
-                    .build()
-            )
-            .build();
+                .build();
 
         buildView();
     }
@@ -120,7 +76,6 @@ public class RagChat extends VerticalLayout {
             var ui = UI.getCurrent();
             chatClient.prompt()
                 .user(prompt)
-                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, chatId))
                 .stream()
                 .content()
                 .subscribe(token -> ui.access(() -> answer.appendText(token)));
@@ -137,10 +92,10 @@ public class RagChat extends VerticalLayout {
         add(inputLayout);
     }
 
-
     void processDocument(Resource resource) {
-        vectorStore.write(
-            new TokenTextSplitter().apply(
-                new TikaDocumentReader(resource).read()));
+        var documentReader = new TikaDocumentReader(resource);
+        var documentTransformer = new TokenTextSplitter();
+        vectorStore.write(documentTransformer.apply(documentReader.read()));
     }
+
 }
